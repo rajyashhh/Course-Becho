698a808167c52c202ac3d320af5a062a9c6c4764
Yash Raj Singh <workmail.yrs@gmail.com>
2025-03-13 04:16:04 +0530

    added signup in admin

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ const app = express();
 const {userRouter}=require("./routes/user")
 const {courseRouter}=require("./routes/course")
 const {adminRouter}=require("./routes/admin")
-
+const mongoose = require("mongoose");
+const port = process.env.port;
+app.use(express.json());
 app.use('/user', userRouter);
 app.use('/course', courseRouter);
 app.use('/admin', adminRouter);
 
-const port = process.env.port;
-app.listen(port, console.log(`Server is starting on local host with port number ${port}`))
+function auth(req,res){
+
+}
+
+
+
+
+
+async function main(){
+    const mongoose_url = process.env.mongo_url;
+    await mongoose.connect(mongoose_url);
+    app.listen(port, console.log(`Server is starting on local host with port number ${port}`))
+}
+
+main();
--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,82 @@
 const {Router}= require("express");
 const adminRouter = Router();
+const bcrypt = require("bcrypt");
+const {adminModel} = require("../db");
+const {z} = require("zod");
+const { ObjectId } = require("mongodb");
+const e = require("express");
+
 
 adminRouter.post('/create',(req,res)=>{
     res.send({
 
     })
 })
-adminRouter.post('/signup', (req,res)=>{
+adminRouter.post('/signup', async (req,res)=>{
+    
+    const requiredBody = z.object({
+        email : z.string().min(5).max(100).email(),
+        password : z.string().min(3,"Password must contain atleast 5 characters").max(20),
+        firstName : z.string().min(3).max(30),
+        lastName : z.string().min(3).max(30)
+    })
+
+    const parsedDatawithSuccess = requiredBody.safeParse(req.body);
+    if(!parsedDatawithSuccess.success){
+        res.json({
+            message : "Incorrect format",
+            error : parsedDatawithSuccess.error
+        })
+        return;
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+
+
+    const existingUser = await adminModel.findOne({ email: email });
+    if (existingUser) {
+        return res.json({
+            message: "You are already signed up!"
+        });
+    }
+    let errorThrown =false;
+    try{
+         await adminModel.create({
+            email : email,
+            password : hashedPassword,
+            firstName : firstName,
+            lastName : lastName
+        })
+        res.json({
+            message : "You have successfully signed up!"
+        })
+    }
+    catch(e){
+        errorThrown = true;
+        console.log(e)
+    }
+    if (errorThrown){
+        res.json({
+            message : "Error in creating admin model!",
+        })
+    }
+})
+
+adminRouter.post('login', (req,res)=>{
     res.send({
 
     })
 })
-
-adminRouter.post('/login', (req,res)=>{
+adminRouter.put('update', (req,res)=>{
     res.send({
 
     })
 })
-adminRouter.put('/update', (req,res)=>{
-    res.send({
-
-    })
-})
-adminRouter.get('/course/bulk', (req,res)=>{
+adminRouter.get('coursebulk', (req,res)=>{
     res.send({
 
     })
