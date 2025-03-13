2a4f20c2733c43298719c1f5345b69ca7578a5ca
Yash Raj Singh <workmail.yrs@gmail.com>
2025-03-13 19:07:31 +0530

    added middlewares

---
+++ middleware/admin.js
@@ -0,0 +1,20 @@
+const jwt = require ("jsonwebtoken");
+const jwt_pass = process.env.JWT_SECRET_KEY_ADMIN;
+
+function authadmin(req, res, next){
+    const token = req.headers.token;
+    const verified = jwt.verify(token, jwt_pass);
+    if(verified){
+        req.userId = verified.id;
+        next()
+    }
+    else{
+        res.json({
+            message: "Admin is not signed in"
+        })
+    }
+}
+
+module.exports={
+    authadmin : authadmin,
+}
